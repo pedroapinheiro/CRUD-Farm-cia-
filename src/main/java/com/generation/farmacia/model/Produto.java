f485f90c725517861939dddda9b6bb5cc776a1e0
unknown <pedro.azevedop18@hotmail.com>
2023-03-20 21:15:31 -0300

    Atividade SPRING Farmacia

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,89 @@
+package com.generation.farmacia.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 5, max = 50, message = "O atributo nome é obrigatório")
+    private String nome;
+
+    @NotBlank
+    @Size(min = 5, max = 50, message = "O atributo marca é obrigatório")
+    private String marca;
+
+    @NotNull(message = "O atributo quantidade é obrigatório")
+    private Integer quantidade;
+
+    @NotNull(message = "O atributo preco é obrigatório")
+    private BigDecimal preco;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setName(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
